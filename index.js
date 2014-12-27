43c90fa59d6419ec99bedf3e3d4323f85160445a
Piotr Plewa <pplewa@atlassian.com>
2014-12-27 16:30:34 +1100

    adding moves integration

--- index.js
+++ index.js
@@ -1,12 +1,38 @@
 var Evernote = require('evernote').Evernote;
+var MovesApi = require('moves-api').MovesApi;
 var Settings = require('settings');
 var Handlebars = require('handlebars');
 var moment = require('moment');
 var fs = require('fs');
 var Q = require('q');
+var polyUtil = require('polyline-encoded');
 var request = require('request');
 var config = new Settings(require('./config'));
 
+// Handlebards helpers
+Handlebars.registerHelper("mins", function(value) {
+	return Math.round(value/60);
+});
+
+Handlebars.registerHelper("config", function(val1, val2) {
+	return config[val1][val2];
+});
+
+//
+var template = fs.readFileSync('template.hbs', { encoding: 'utf-8' });
+var getTemplate = Handlebars.compile(template);
+
+//
+var client = new Evernote.Client({
+	token: config.evernote.accessToken,
+	sandbox: config.evernote.sandbox
+});
+var noteStore = client.getNoteStore();
+
+// 
+var moves = new MovesApi(config.moves);
+
+/*
 function getToken() {
 	var client = new Evernote.Client({
 		consumerKey: config.evernote.key,
@@ -18,8 +44,10 @@ function getToken() {
 		// and then redirect to client.getAuthorizeUrl(oauthToken)
 	});
 }
+*/
 
-function makeNote(noteStore, noteTitle, noteBody, parentNotebook, callback) {
+function makeNote(noteTitle, noteBody, parentNotebook) {
+	var deferred = Q.defer();
 	var nBody = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>";
 	nBody += "<!DOCTYPE en-note SYSTEM \"http://xml.evernote.com/pub/enml2.dtd\">";
 	nBody += "<en-note>" + noteBody + "</en-note>";
@@ -39,29 +67,21 @@ function makeNote(noteStore, noteTitle, noteBody, parentNotebook, callback) {
 	}
  
 	// Attempt to create note in Evernote account
-	noteStore.createNote(ourNote, function(err, note) {
-		if (err) {
+	noteStore.createNote(ourNote, function(error, note) {
+		if (error) {
+			console.log(arguments);
+			deferred.reject(new Error(error));
 			// Something was wrong with the note data
 			// See EDAMErrorCode enumeration for error code explanation
 			// http://dev.evernote.com/documentation/reference/Errors.html#Enum_EDAMErrorCode
-			console.log(err);
 		} else {
-			callback(note);
+			deferred.resolve(note);
 		}
 	});
+	return deferred.promise;	
 }
 
-
-var template = fs.readFileSync('template.hbs', { encoding: 'utf-8' });
-var getTemplate = Handlebars.compile(template);
-
-var client = new Evernote.Client({
-	token: config.evernote.accessToken,
-	sandbox: config.evernote.sandbox
-});
-var noteStore = client.getNoteStore();
-
-function getMemories(noteStore) {
+function getMemories() {
 	var deferred = Q.defer();
 	var filter = new Evernote.NoteFilter({
 		words: 'notebook:journal intitle:' + moment().date(moment().date()-1).format('DD/MM/'),
@@ -75,7 +95,14 @@ function getMemories(noteStore) {
 		if (error) {
 			deferred.reject(new Error(error));
 		} else {
-			deferred.resolve(data.notes);
+			var memories = [];
+			data.notes.forEach(function(note){
+				memories.push({
+					link: 'evernote:///view/' + config.evernote.userId + '/' + config.evernote.shardId + '/' + note.guid + '/' + note.guid + '/',
+					title: note.title
+				});
+			});
+			deferred.resolve(memories);
 		}
 	});
 	return deferred.promise;
@@ -120,21 +147,96 @@ function getMappiness() {
 }
 
 
-Q.all([getMemories(noteStore), getMappiness()]).spread(function(notes, mappiness){
-	var memories = [];
-	notes.forEach(function(note){
-		memories.push({
-			link: 'evernote:///view/' + config.evernote.userId + '/' + config.evernote.shardId + '/' + note.guid + '/' + note.guid + '/',
-			title: note.title
-		});
-	});
+function getStoryline() {
+	var deferred = Q.defer();
+	moves.getStoryline({ trackPoints: true, date: moment().subtract(1, 'day') }, function(error, storylines) {
+		if (error) {
+			deferred.reject(new Error(error));
+		} else {
+			var storyline = {
+				summary: storylines[0].summary,
+				segments: []
+			};
 
+			storylines[0].segments.forEach(function(segment, i){
+				if (segment.place) {
+					var temp = {
+						start: moment(segment.startTime, 'YYYYMMDDTHms').format('HH:mm'),
+						end: moment(segment.endTime, 'YYYYMMDDTHms').format('HH:mm')
+					};
+					temp.activity = segment.activities[0].activity;
+					if (segment.activities.length === 1) {
+						temp.duration = segment.activities[0].duration;
+						temp.distance = segment.activities[0].distance;
+						temp.steps = segment.activities[0].steps;
+						temp.calories = segment.activities[0].calories;
+					} else if (segment.activities.length > 1) {
+						temp.duration = segment.activities.reduce(function(a, b){ return (a.duration || 0) + (b.duration || 0); });
+						temp.distance = segment.activities.reduce(function(a, b){ return (a.distance || 0) + (b.distance || 0); });
+						temp.steps = segment.activities.reduce(function(a, b){ return (a.steps || 0) + (b.steps || 0); });
+						temp.calories = segment.activities.reduce(function(a, b){ return (a.calories || 0) + (b.calories || 0); });
+					}
+					if (i === 0) temp.start = '00:00';
+					if (i === storylines[0].segments.length - 1) temp.end = '00:00';
+					temp.place = {
+						name: segment.place.name,
+						lon: segment.place.location.lon,
+						lat: segment.place.location.lat,
+						foursquareId: segment.place.foursquareId,
+						type: segment.place.type
+					};
+					storyline.segments.push(temp);
+				} else {
+					var path = '', start, finish;
+					segment.activities.forEach(function(activity){
+						var temp = {
+							start: moment(activity.startTime, 'YYYYMMDDTHms').format('HH:mm'),
+							end: moment(activity.endTime, 'YYYYMMDDTHms').format('HH:mm'),
+							activity: activity.activity,
+							duration: activity.duration,
+							distance: activity.distance,
+							calories: activity.calories,
+							steps: activity.steps
+						};
+						if (activity.trackPoints) {
+							start = {
+								lat: activity.trackPoints[0].lat,
+								lon: activity.trackPoints[0].lon
+							}
+							finish = {
+								lat: activity.trackPoints[activity.trackPoints.length-1].lat,
+								lon: activity.trackPoints[activity.trackPoints.length-1].lon
+							}
+							var path = polyUtil.encode(activity.trackPoints.map(function(point){
+								return [point.lat, point.lon];								
+							}));
+						}
+						temp.route = {
+							path: 'enc:' + path,
+							start: start,
+							finish: finish
+						};
+						storyline.segments.push(temp);
+					});
+				}
+
+				deferred.resolve(storyline);
+			});
+		}
+	});
+	return deferred.promise;
+}
+
+Q.all([getMemories(), getMappiness(), getStoryline()]).spread(function(memories, mappiness, storyline){
 	var noteTitle = moment().date(moment().date()-1).format('DD/MM/YYYY ddd');
 	var noteBody = getTemplate({ 
 		memories: memories,
-		mappiness: mappiness
+		mappiness: mappiness,
+		storyline: storyline
 	});
-	makeNote(noteStore, noteTitle, noteBody, null, function(err, note){
+	makeNote(noteTitle, noteBody).then(function(note){
 		console.log('ok');
 	});
+}).fail(function(){
+	console.log(arguments);	
 });
